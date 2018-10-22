cd5ccefe83dc7a1526590f8525f1cf465a517d9f
marianne pearson <marianne.pearson@digital.hmrc.gov.uk>
2018-10-22 15:27:23 +0100

    [APB-3051][MP] Changing tab styling on MYTA page, rearrangin to put pending authorisations on one tab. RIP tomato

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -24,4 +24,19 @@ $(function() {
     //Add aria-hidden to hidden inputs
     $('[type="hidden"]').attr("aria-hidden", true)
 
+
+    function nodeListForEach (nodes, callback) {
+        if (window.NodeList.prototype.forEach) {
+            return nodes.forEach(callback)
+        }
+        for (var i = 0; i < nodes.length; i++) {
+            callback.call(window, nodes[i], i, nodes)
+        }
+    }
+
+    var $tabs = document.querySelectorAll('[data-module="tabs"]')
+    nodeListForEach($tabs, function ($tabs) {
+        new window.GOVUKFrontend($tabs).init()
+    })
+
 });
