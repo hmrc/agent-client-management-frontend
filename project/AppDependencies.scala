ec446d449a11dec8ac437ff4496fbe9171f5e547
max-hmrc <max.ashton@digital.hmrc.gov.uk>
2023-04-26 10:24:59 +0100

    APB-7162 fix CSP issues for pen test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,15 @@ import sbt._
 object AppDependencies {
 
   val mongoVersion = "0.74.0"
+  val bootstrapVersion = "7.12.0"
 
   lazy val compileDeps = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.12.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc"         % "5.5.0-play-28",
     "uk.gov.hmrc" %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc" %% "agent-kenshoo-monitoring"   % "4.8.0-play-28",
     "uk.gov.hmrc" %% "agent-mtd-identifiers"      % "0.55.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"      % mongoVersion
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"   % mongoVersion
   )
 
   def testDeps(scope: String) = Seq(
