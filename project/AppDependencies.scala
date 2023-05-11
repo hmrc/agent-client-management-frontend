76f944b62efdd2c2afcb02721f5d3bd7c9787a36
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2023-05-11 10:37:25 +0100

    [CS] play-frontend-hmrc to 7.7

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,8 +7,8 @@ object AppDependencies {
 
   lazy val compileDeps = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.5.0-play-28",
-    "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.7.0-play-28",
+    "uk.gov.hmrc"       %% "play-partials"              % "8.4.0-play-28",
     "uk.gov.hmrc"       %% "agent-kenshoo-monitoring"   % "5.3.0",
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.2.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % mongoVersion
