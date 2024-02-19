c0280568e24ece300983fc6dfc7a7e3db58dac6e
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2024-02-19 22:05:41 +0000

    APB-7780[CS] tudor crown update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
@@ -7,7 +7,7 @@ object AppDependencies {
 
   lazy val compileDeps = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "7.23.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-28" % "8.5.0",
     "uk.gov.hmrc"       %% "play-partials"              % "8.4.0-play-28",
     "uk.gov.hmrc"       %% "agent-kenshoo-monitoring"   % "5.5.0",
     "uk.gov.hmrc"       %% "agent-mtd-identifiers"      % "1.15.0",
